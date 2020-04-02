5a84ecbf8dcfc2fe749c85e54544efdc817bf416
OlivierLdcq <olivier.leducq@hotmail.fr>
2020-04-02 19:07:12 +0200

    upgrade

---
+++ script.js
@@ -0,0 +1,14 @@
+var _ = require('lodash');
+
+const array = [1,2,3,4,5,6,7,8];
+
+console.log(_.without(array,3));
+
+
+const title = document.querySelector(".h1");
+
+title.addEventListener('click', function(e) {
+
+	console.log("click");
+	
+});
